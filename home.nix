826782891dd7b42dbbcfcd589d2762d0310898a5
Dave King <dave@davbo.uk>
2019-05-18 14:37:45 +0100

    Add spacemacs

--- home.nix
+++ home.nix
@@ -11,16 +11,15 @@
 
   programs.emacs = {
     enable = true;
-    extraPackages = epkgs: with epkgs; [ 
-      use-package
-
-      evil evil-collection evil-surround
-
-      ivy counsel swiper
-
-      magit 
-    ];
   };
+  home.file.".emacs.d" = {
+    source = builtins.fetchGit {
+      url = "https://github.com/syl20bnr/spacemacs";
+      ref = "develop";
+    };
+    recursive = true;
+  };
+  home.file.".spacemacs".source = ./spacemacs.el;
 
   programs.git = {
     enable = true;
