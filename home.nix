df620730af4a5f28e25940135c61429e7ddc5ed9
Dave King <dave@davbo.uk>
2019-05-31 15:19:41 +0100

    Moving back to bash

--- home.nix
+++ home.nix
@@ -1,61 +1,98 @@
 { pkgs, ... }:
 
-{
+let
+  color_ps1 = "PS1='\${debian_chroot:+($debian_chroot)}\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\\$ '";
+  plain_ps1 = "PS1='\${debian_chroot:+($debian_chroot)}\\u@\\h:\\w\\$ '";
+in {
   home.packages = with pkgs; [
-    bash
-    zsh
-
-    coreutils
-    findutils
-
-    gnugrep
-    gnused
-
     # Fonts
     fira-code
     fira-code-symbols
     source-code-pro
   ];
 
-  programs.zsh = {
+  programs.bash = {
     enable = true;
     sessionVariables = {
-      NIX_PATH = "$HOME/.nix-defexpr/channels\${NIX_PATH:+:}$NIX_PATH";
       GOPATH = "$HOME/go";
       PATH = "$PATH:$HOME/.nix-profile/bin:$GOPATH/bin";
+      XDG_DATA_DIRS = "$HOME/.nix-profile/share:$HOME/.share:\${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}";
       _JAVA_AWT_WM_NONREPARENTING = 1;
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=8";
-      PURE_PROMPT_SYMBOL="$";
     };
-    initExtra = ''
-      autoload -Uz promptinit 
-      promptinit
-      . $HOME/.zsh/plugins/pure/pure.zsh
-      . $HOME/.nix-profile/etc/profile.d/nix.sh
-      export XDG_DATA_DIRS=$HOME/.nix-profile/share:$HOME/.share:"''${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}"
+    shellAliases = {
+      du = "du -h";
+      df = "df -h";
+      ls = "ls --color=tty";
+      ll = "ls -al";
+    };
+    profileExtra = ''
+    # Source default profile
+    if [ -f /etc/profile ] ; then
+      . /etc/profile
+    fi
     '';
-    plugins = [
-      {
-        # will source zsh-autosuggestions.plugin.zsh
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-autosuggestions";
-          rev = "v0.5.2";
-          sha256 = "1xhrdv6cgmq9qslb476rcs8ifw8i2vf43yvmmscjcmpz0jac4sbx";
-        };
-      }
-      {
-        name = "pure";
-        file = "async.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "sindresorhus";
-          repo = "pure";
-          rev = "v1.9.0";
-          sha256 = "0qimbksjn7w35wqfm40sjhwkx843m1szqmwlmw3xxf69yx1hyz6w";
-        };
-      }
-    ];
+    initExtra = ''
+    # Get home-manager working
+    export NIX_PATH=$HOME/.nix-defexpr/channels''${NIX_PATH:+:}$NIX_PATH
+
+    # Copied from Ubuntu's default bashrc
+    # make less more friendly for non-text input files, see lesspipe(1)
+    [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+    # set variable identifying the chroot you work in (used in the prompt below)
+    if [ -z "''${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+        debian_chroot=$(cat /etc/debian_chroot)
+    fi
+
+    # set a fancy prompt (non-color, unless we know we "want" color)
+    case "$TERM" in
+        xterm-color|*-256color) color_prompt=yes;;
+    esac
+
+    # uncomment for a colored prompt, if the terminal has the capability; turned
+    # off by default to not distract the user: the focus in a terminal window
+    # should be on the output of commands, not on the prompt
+    #force_color_prompt=yes
+
+    if [ -n "$force_color_prompt" ]; then
+        if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+            # We have color support; assume it's compliant with Ecma-48
+            # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+            # a case would tend to support setf rather than setaf.)
+            color_prompt=yes
+        else
+            color_prompt=
+        fi
+    fi
+
+    if [ "$color_prompt" = yes ]; then
+        ${color_ps1}
+    else
+        ${plain_ps1}
+    fi
+    unset color_prompt force_color_prompt
+
+    # If this is an xterm set the title to user@host:dir
+    case "$TERM" in
+    xterm*|rxvt*)
+        PS1="\[\e]0;''${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+        ;;
+    *)
+        ;;
+    esac
+
+    # enable programmable completion features (you don't need to enable
+    # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+    # sources /etc/bash.bashrc).
+    if ! shopt -oq posix; then
+      if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+      elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+      fi
+    fi
+
+    '';
   };
 
   programs.ssh.enable = true;
@@ -73,8 +110,13 @@
 
   programs.git = {
     enable = true;
+    package = pkgs.gitAndTools.gitFull;
     userName = "Dave King";
     userEmail = "dave@davbo.uk";
+    extraConfig = ''
+    [color]
+    ui = true
+    '';
   };
 
   programs.home-manager = {
